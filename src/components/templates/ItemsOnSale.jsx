d9635ea7ca4370d059a77062d9c831f4eb436fef
yyyljy <yyyljy@gmail.com>
2022-11-02 11:26:49 +0900

    refactoring

--- src/components/templates/ItemsOnSale.jsx
+++ src/components/templates/ItemsOnSale.jsx
@@ -1,44 +1,9 @@
 import styled from "styled-components";
 import * as colors from "@styles/colors";
-
-const Container = styled.div`
-  width: 100%;
-  height: 394px;
-  padding: 40px 16px;
-  background-color: white;
-`;
-
-const SectionTopWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const SectionTitle = styled.span`
-  font-size: 18px;
-  font-weight: 700;
-`;
-
-const ShowAllText = styled.span`
-  font-size: 14px;
-  color: ${colors.textSecondary};
-  margin-right: 4px;
-`;
-
-const EventCardsWrapper = styled.div`
-  margin-top: 24px;
-  display: flex;
-  gap: 16px;
-
-  overflow-x: scroll;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-`;
+import Ether from "@components/atoms/Ether";
+import SectionLayout from "@components/molecules/SectionLayout";
+import SectionTop from "@components/molecules/SectionTop";
+import HideScrollX from "@components/molecules/HideScrollX";
 
 const CardWrapper = styled.div`
   border-radius: 16px;
@@ -69,6 +34,9 @@ const PriceBox = styled.div`
 const CollectionTitle = styled.div`
   font-size: 12px;
   color: ${colors.textSecondary};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const Title = styled.div`
@@ -77,28 +45,49 @@ const Title = styled.div`
   margin-top: 4px;
 `;
 
-const PriceTitle = styled.div``;
+const PriceTitle = styled.div`
+  font-size: 10px;
+  color: ${colors.textSecondary};
+  font-weight: 700;
+`;
+
+const PriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-top: 4px;
+`;
+
+const PriceText = styled.div`
+  font-family: MarkPro-Heavy;
+  font-size: 14px;
+`;
 
 function ItemsOnSale() {
+  const collectionTitle = "FrankenPunksFrankenPunksFrankenPunksFrankenPunks";
+
   return (
-    <Container>
-      <SectionTopWrapper>
-        <SectionTitle>지금 판매중인 아이템</SectionTitle>
-        <ShowAllText>아이템 전체보기</ShowAllText>
-      </SectionTopWrapper>
-      <EventCardsWrapper>
-        <CardWrapper>
-          <CardImage src="https://konkrit-prod-itemmedia-t837t51tz51i.s3.ap-northeast-2.amazonaws.com/0x1fec856e25f757fed06eb90548b0224e91095738/0x1fec856e25f757fed06eb90548b0224e91095738-6741.png"></CardImage>
-          <InfoBox>
-            <CollectionTitle>FrankenPunks</CollectionTitle>
-            <Title>#6741</Title>
-          </InfoBox>
-          <PriceBox>
-            <PriceTitle></PriceTitle>
-          </PriceBox>
-        </CardWrapper>
-      </EventCardsWrapper>
-    </Container>
+    <SectionLayout>
+      <SectionTop title={"지금 판매중인 아이템"} showAll={"아이템 전체보기"} />
+      <HideScrollX>
+        {[1, 2, 3, 4, 5].map((num) => (
+          <CardWrapper>
+            <CardImage src="https://konkrit-prod-itemmedia-t837t51tz51i.s3.ap-northeast-2.amazonaws.com/0x1fec856e25f757fed06eb90548b0224e91095738/0x1fec856e25f757fed06eb90548b0224e91095738-6741.png"></CardImage>
+            <InfoBox>
+              <CollectionTitle>{collectionTitle}</CollectionTitle>
+              <Title>#6741</Title>
+            </InfoBox>
+            <PriceBox>
+              <PriceTitle>판매가</PriceTitle>
+              <PriceWrapper>
+                <Ether />
+                <PriceText>0.02</PriceText>
+              </PriceWrapper>
+            </PriceBox>
+          </CardWrapper>
+        ))}
+      </HideScrollX>
+    </SectionLayout>
   );
 }
 
