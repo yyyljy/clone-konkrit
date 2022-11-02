515c13d96b3096aae400c10f908bf8c448538702
yyyljy <yyyljy@gmail.com>
2022-11-02 15:39:47 +0900

    푸터  퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import GlobalStyle from "./GlobalStyle";
-import styled from "styled-components";
 import Header from "@components/templates/Header";
-import Events from "@components/templates/Events";
 import TopBanner from "@components/templates/TopBanner";
+import Events from "@components/templates/Events";
 import ItemsOnSale from "@components/templates/ItemsOnSale";
 import OpenseaTopCollections from "@components/templates/OpenseaTopCollections";
-
-const OnBoarding = styled.div`
-  width: 100%;
-  height: 415px;
-  background-color: teal;
-`;
-
-const Footer = styled.footer`
-  height: 240px;
-  width: 100%;
-  background-color: floralwhite;
-  margin-top: 100px;
-`;
+import OnBoarding from "@components/templates/OnBoarding";
+import Footer from "@components/templates/Footer";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header>헤더부분</Header>
-      <TopBanner />
+      <TopBanner>탑 배너</TopBanner>
       <Events />
       <ItemsOnSale>판매중인 아이템</ItemsOnSale>
       <OpenseaTopCollections>많이 거래된 컬렉션</OpenseaTopCollections>
