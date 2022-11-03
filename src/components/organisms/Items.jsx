950c73cd128608723b7d383e957353c947858285
yyyljy <yyyljy@gmail.com>
2022-11-03 11:17:58 +0900

    Items.jsx LoadingSpinner Added

--- src/components/organisms/Items.jsx
+++ src/components/organisms/Items.jsx
@@ -2,6 +2,16 @@ import HideScrollX from "@components/molecules/HideScrollX";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Item from "@components/molecules/Item";
+import LoadingSpinner from "@components/atoms/LoadingSpinner";
+import styled from "styled-components";
+
+const LoadingWrapper = styled.div`
+  height: 275px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Items() {
   const [items, setItems] = useState({ items: [] });
@@ -24,7 +34,11 @@ export default function Items() {
     fetchItems();
   }, []);
   if (isLoading) {
-    return <div>로딩중</div>;
+    return (
+      <LoadingWrapper>
+        <LoadingSpinner />
+      </LoadingWrapper>
+    );
   }
   if (isError) {
     return <div>에러</div>;
